763b10d4baba15992665ea1c73e184b386ac0132
e113s <e113s10@gmail.com>
2019-06-06 12:41:03 -0500

    Codigo Pool Connection

---
+++ PoolConnectionSample/ContextDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PoolConnectionSample
+{
+    class ContextDAO
+    {
+        private SqlConnection obj_sqlConn = new SqlConnection();
+        private SqlConnectionStringBuilder obj_sqnBuild = new SqlConnectionStringBuilder();
+
+        public ContextDAO()
+        {
+            obj_sqnBuild.DataSource = "localhost";
+            obj_sqnBuild.UserID = "elles";
+            obj_sqnBuild.Password = "54321";
+            obj_sqnBuild.InitialCatalog = "PlantAndHealth";
+            obj_sqnBuild.MinPoolSize = 1;
+            obj_sqnBuild.MinPoolSize = 25;
+            obj_sqnBuild.ConnectTimeout = 10;
+            obj_sqnBuild.Pooling = true;
+            //obj_sqnBuild.IntegratedSecurity = true;
+            obj_sqlConn.ConnectionString = obj_sqnBuild.ConnectionString;
+        }
+
+        public void openSqlConn(SqlConnection connection)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+
+            connection.Open();
+        }
+
+        public SqlConnection sConNme//Propert for retrieving the sql connection string
+        {
+            get
+            {
+                return obj_sqlConn;
+                //return Obj_odnbuild.ConnectionString;
+            }
+            set
+            {
+                sConNme = obj_sqlConn;
+            }
+        }
+    }
+}
---
+++ PoolConnectionSample/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PoolConnectionSample
+{
+    class Program
+    {
+        private static ContextDAO context = new ContextDAO();
+        private static SqlCommand command = new SqlCommand();
+        private static SqlDataReader reader;
+
+        static void Main(string[] args)
+        {
+
+            /*
+            string sqlConnection = "Server= localhots; User Id=elles; Password=54321;" +
+                "Integrated security = SSPI; Database = PlantAndHealth";
+            */
+
+            /*connection.ConnectionString = sqlConnection + "Connection Timeout = 30;" +
+                "Connection Lifetime = 0; Min Pool Size = 0; Max Pool Size = 100; Pooling = true";
+            */
+            
+
+            try
+            {
+
+                Console.WriteLine("==================== Connection 1 =========================");
+
+                command.CommandText = "SELECT TOP 100 * FROM ARTICULOS";
+                command.CommandType = CommandType.Text;
+                command.Connection = context.sConNme;
+
+                context.openSqlConn(context.sConNme);
+
+                using (reader = command.ExecuteReader())
+                {
+                    Console.WriteLine("Codigo Producto\tNombre\t\tFamilia\t\tPrecio Unitario\t\tCosto Unitario");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0} \t | {1} \t | {2} \t | {3} \t | {4}",
+                        reader[0], reader[1], reader[2], reader[3], reader[4]));
+                    }
+               
+                }
+
+                reader.Close();
+               
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+                reader.Close();
+            }
+
+
+            try
+            {
+                Console.WriteLine("==================== Connection 2 =========================");
+
+                command.CommandText = "SELECT * FROM REGION";
+                command.CommandType = CommandType.Text;
+                command.Connection = context.sConNme;
+
+                context.openSqlConn(context.sConNme);
+
+                using (reader = command.ExecuteReader())
+                {
+                    Console.WriteLine("REGION ID\t\tREGION");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0} \t | {1}",
+                        reader[0], reader[1]));
+                    }
+
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+                reader.Close();
+            }
+
+            try
+            {
+                Console.WriteLine("==================== Connection 3 =========================");
+
+                command.CommandText = "SELECT * FROM COMUNA;";
+                command.CommandType = CommandType.Text;
+                command.Connection = context.sConNme;
+
+                context.openSqlConn(context.sConNme);
+
+                using (reader = command.ExecuteReader())
+                {
+                    Console.WriteLine("COMUNA ID\t\tCOMUNA NOMBRE\t\tPROVINCIA ID");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0} \t | {1} \t | {2}",
+                        reader[0], reader[1], reader[2]));
+                    }
+                }
+
+                reader.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+                reader.Close();
+            }
+        }
+    }
+}
